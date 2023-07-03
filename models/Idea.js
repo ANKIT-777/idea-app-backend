4923adc58c7afaa8aae9c5599bb3168117131c82
Ankit Sharma <an2ki4t5s@gmail.com>
2023-07-03 22:40:55 +0530

    added idea and user componnent

---
+++ models/Idea.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const ideaSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Idea = mongoose.model('Idea', ideaSchema);
+
+module.exports = Idea;
